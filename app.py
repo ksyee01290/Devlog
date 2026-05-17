01815867fe6f81bbad46ad95b6378bbca8c47a8f
sourcejung <sourcejung@gmail.com>
2026-05-18 03:43:11 +0900

    CRUD 구현

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///devlog.db"
@@ -15,9 +16,8 @@ app.secret_key = "123123"
 
 @app.route("/")
 def hello():
-    name = "김철수"
-    posts = ["첫번째 글", "두번째 글", "세번째 글"]
-    return render_template("index.html", name=name, posts=posts)
+    posts = Post.query.all()
+    return render_template("index.html", posts=posts)
 
 @app.route("/test")
 def test():
@@ -25,12 +25,46 @@ def test():
     db.session.add(new_post)
     db.session.commit()
     return "데이터 추가 완료!"
+# 글쓰기
+@app.route("/write", methods=["GET", "POST"])
+def write():
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
+        date = datetime.now().strftime("%Y-%m-%d | %H:%M:%S")
+        new_post = Post(title=title, content=content, date=date)
+        db.session.add(new_post)
+        db.session.commit()
+        flash("글이 작성되었습니다.")
+        return redirect(url_for("posts"))
+    return render_template("write.html")
 
 # 조회
 @app.route("/posts")
 def posts():
     posts = Post.query.all()
     return render_template("index.html", posts=posts)
+
+# 수정
+@app.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit(id):
+    post = Post.query.get(id)
+    if request.method == "POST":
+        post.title = request.form["title"]
+        post.content = request.form["content"]
+        db.session.commit()
+        flash("글이 수정되었습니다!")
+        return redirect(url_for("hello"))
+    return render_template("edit.html", post=post)
+
+# 삭제
+@app.route("/delete/<int:id>")
+def delete(id):
+    post = Post.query.get(id)
+    db.session.delete(post)
+    db.session.commit()
+    flash("글이 삭제되었습니다!")
+    return redirect(url_for("hello"))
 
 @app.route("/user/<username>")
 def user(username):
