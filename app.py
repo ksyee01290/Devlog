7b128b0afcd253f3e7dec40d2294101fc68bc933
sourcejung <sourcejung@gmail.com>
2026-05-18 00:01:29 +0900

    템플릿 상속

--- app.py
+++ app.py
@@ -4,10 +4,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
+    name = "김철수"
+    posts = ["첫번째 글", "두번째 글", "세번째 글"]
+    return render_template("index.html", name=name, posts=posts)
 
 @app.route("/about")
 def about():
     return "소개 페이지"
+
+@app.route("/user/<username>")
+def user(username):
+    return render_template("index.html", name=username, posts=[])
 
 app.run()
